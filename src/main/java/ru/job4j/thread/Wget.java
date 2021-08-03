b920baffca42fc7a488c459018fa6a7df9bbeb59
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-08-03 23:03:32 +0300

    4. Скачивание файла с ограничением. [#144271 #184969] рефакторинг кода

--- src/main/java/ru/job4j/thread/Wget.java
+++ src/main/java/ru/job4j/thread/Wget.java
@@ -3,40 +3,38 @@ package ru.job4j.thread;
 import java.io.BufferedInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.time.LocalTime;
 
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
-    private long delay = 0L;
+    private final String destFileName;
+    private int delay = 0;
 
-    public Wget(String url, int speed) {
+    public Wget(String url, int speed, String destFileName) {
         this.url = url;
         this.speed = speed;
+        this.destFileName = destFileName;
     }
 
     @Override
     public void run() {
         try (BufferedInputStream in = new BufferedInputStream(
                 new URL(url).openStream());
-             FileOutputStream fileOutputStream = new FileOutputStream("pom_tmp.xml")) {
+             FileOutputStream fileOutputStream = new FileOutputStream(destFileName)) {
             byte[] dataBuffer = new byte[1024];
             int bytesRead;
-            //Переводим миллисекунды в наносекунды:
-            long nanoSpeed = speed * 1000000L;
-            long startTime = System.nanoTime();
+            int nanoSpeed = speed;
+            int startTime = (int) (System.nanoTime() / 1000000);
             while ((bytesRead = in.read(dataBuffer, 0, 1024)) != -1) {
-                long estimatedTime = System.nanoTime() - startTime;
+                int estimatedTime = (int) (System.nanoTime() / 1000000) - startTime;
                 System.out.printf(
-                        "Время загрузки: %d нс. Загружено байт: %d%n", estimatedTime, bytesRead);
-                //Определяем задержку в наносекундах:
+                        "Время загрузки: %d мс. Загружено байт: %d%n", estimatedTime, bytesRead);
                 delay = estimatedTime < nanoSpeed ? nanoSpeed - estimatedTime : 0;
-                System.out.printf("Установлено время задержки: %d нс.%n%n", delay);
+                System.out.printf("Установлено время задержки: %d мс.%n%n", delay);
                 fileOutputStream.write(dataBuffer, 0, bytesRead);
-                Thread.sleep(delay / 1000000);
-                startTime = System.nanoTime();
+                Thread.sleep(delay);
+                startTime = (int) (System.nanoTime() / 1000000);
             }
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
@@ -44,10 +42,18 @@ public class Wget implements Runnable {
     }
 
     public static void main(String[] args) throws InterruptedException {
+        if (args.length != 3) {
+            throw new IllegalArgumentException("Invalid input args. Usage 3 args: url, speed, destfilename.");
+        }
         String url = args[0];
-        //Скорость загрузки 1 байта в миллисекундах:
-        int speed = Integer.parseInt(args[1]);
-        Thread wget = new Thread(new Wget(url, speed));
+        int speed;
+        try {
+            speed = Integer.parseInt(args[1]);
+        } catch (Exception e) {
+            throw new IllegalArgumentException("Invalid speed argument. Speed must be a number.");
+        }
+        String destFileName = args[2];
+        Thread wget = new Thread(new Wget(url, speed, destFileName));
         wget.start();
         wget.join();
     }
