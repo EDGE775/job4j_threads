f88962095864e6142f516f92433c0e095bb4ed94
EDGE775 <48449257+EDGE775@users.noreply.github.com>
2021-08-18 08:07:38 +0300

    3. ForkJoinPool [#315067 #190850] рефакторинг кода

--- src/main/java/ru/job4j/pool/ParallelSearchIndex.java
+++ src/main/java/ru/job4j/pool/ParallelSearchIndex.java
@@ -7,12 +7,13 @@ import java.util.concurrent.RecursiveTask;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class ParallelSearchIndex<T> extends RecursiveTask<List<Integer>> {
+public class ParallelSearchIndex<T> extends RecursiveTask<Integer> {
 
     private final T[] array;
     private final int from;
     private final int to;
     private final T obj;
+    private final int maxArrayLength = 10;
 
     public ParallelSearchIndex(T[] array, T obj, int from, int to) {
         this.array = array;
@@ -22,9 +23,9 @@ public class ParallelSearchIndex<T> extends RecursiveTask<List<Integer>> {
     }
 
     @Override
-    protected List<Integer> compute() {
+    protected Integer compute() {
         int size = to - from + 1;
-        if (size < 11) {
+        if (size <= maxArrayLength) {
             return lineFind();
         }
         int mid = (from + to) / 2;
@@ -32,27 +33,27 @@ public class ParallelSearchIndex<T> extends RecursiveTask<List<Integer>> {
         ParallelSearchIndex<T> rightSearch = new ParallelSearchIndex<>(array, obj, mid + 1, to);
         leftSearch.fork();
         rightSearch.fork();
-        List<Integer> left = leftSearch.join();
-        List<Integer> right = rightSearch.join();
-        return Stream.concat(left.stream(), right.stream())
-                .collect(Collectors.toList());
+        Integer left = leftSearch.join();
+        Integer right = rightSearch.join();
+        return Math.max(left, right);
     }
 
-    private List<Integer> lineFind() {
-        List<Integer> indexes = new ArrayList<>();
+    private int lineFind() {
+        int result = -1;
         for (int i = from; i <= to; i++) {
             if (array[i].equals(obj)) {
-                indexes.add(i);
+                result = i;
+                break;
             }
         }
-        return indexes;
+        return result;
     }
 
     public static void main(String[] args) {
         Integer[] array = new Integer[]{1, 3, 5, 7, 23, 23, 65, 34, 74, 5, 79, 6, 4, 56, 34, 5, 345, 2, 35, 37, 45, 75, 8, 6};
         ForkJoinPool forkJoinPool = new ForkJoinPool();
         ParallelSearchIndex<Integer> parallelSearchIndex
-                = new ParallelSearchIndex<>(array, 5, 0, array.length - 1);
+                = new ParallelSearchIndex<>(array, 79, 0, array.length - 1);
         System.out.println(forkJoinPool.invoke(parallelSearchIndex));
     }
 }
